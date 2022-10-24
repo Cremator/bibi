912cbf6902a8a96aaa273a892a929620cdc667fc
Georgi Chompalov <7721127+Cremator@users.noreply.github.com>
2022-10-24 16:19:58 +0300

    Create xmrig.sh

---
+++ xmrig.sh
@@ -0,0 +1,1 @@
+/usr/bin/screen -d -m -S xmrig /root/xmrig-6.18.1/xmrig -o pool.supportxmr.com:443 -u 4AQkNp2B722YyzwBk1KhyCioNFWQCxee4EKfuFYfuK8Tg5G4iMNnoCSCipVegMCropBbM1ChBuerrhDhbMkrvbUKPVd1nbz -k --tls -p 81
